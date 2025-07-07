8f43e877b6a1e35b4ea10fd98d16b944523bb03f
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-07-07 11:05:09 +0300

    chore/adding icons plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import Components from 'unplugin-vue-components/vite'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
     tailwindcss(),
+    Components({ resolvers: [IconsResolver()] }),
+    Icons({ defaultClass: 'size-6' })
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
