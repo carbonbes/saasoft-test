12eedcea20db2355e36c74a7c5bb2460f1949815
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-07-07 11:15:06 +0300

    feat/base button

---
+++ src/components/shared/Base/BaseButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <Primitive
+    v-bind="forwardedProps"
+    class="cursor-pointer disabled:opacity-50 disabled:pointer-events-none"
+  >
+    <slot />
+  </Primitive>
+</template>
+
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps, useForwardProps } from 'reka-ui'
+
+export type BaseButtonProps = PrimitiveProps
+
+const props = withDefaults(defineProps<BaseButtonProps>(), {
+  as: 'button',
+})
+
+const forwardedProps = useForwardProps(props)
+</script>
