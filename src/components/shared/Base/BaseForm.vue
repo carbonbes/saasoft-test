fff304b0b32d56fb06c507710aea984ea854385d
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-07-07 11:18:35 +0300

    feat/base form

---
+++ src/components/shared/Base/BaseForm.vue
@@ -0,0 +1,17 @@
+<template>
+  <Primitive
+    v-bind="forwardedProps"
+    class="ring-2 ring-gray-200 hover:ring-blue-300 focus:ring-blue-500 disabled:opacity-25 disabled:pointer-events-none outline-none"
+  >
+    <slot />
+  </Primitive>
+</template>
+
+<script setup lang="ts">
+import { Primitive, type PrimitiveProps, useForwardProps } from 'reka-ui'
+
+export type BaseFormProps = PrimitiveProps
+
+const props = defineProps<BaseFormProps>()
+const forwardedProps = useForwardProps(props)
+</script>
--- src/components/shared/Base/index.ts
+++ src/components/shared/Base/index.ts
@@ -1,1 +1,2 @@
 export { default as BaseButton, type BaseButtonProps } from './BaseButton.vue'
+export { default as BaseForm, type BaseFormProps } from './BaseForm.vue'
