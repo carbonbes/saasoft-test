52695a55b038f0c489a4fb377132503453665575
Pavel <76450479+carbonbes@users.noreply.github.com>
2025-07-07 11:53:26 +0300

    chore/adding formkit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import './assets/main.css'
 import './assets/tailwind.css'
+import { plugin, defaultConfig } from '@formkit/vue'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-const app = createApp(App)
-
-app.use(createPinia())
+const app = createApp(App).use(createPinia()).use(plugin, defaultConfig)
 
 app.mount('#app')
